10e40eb7a22624c0e6196cd97b26ea31a7e253a5
barimehdi77 <Barimehdi777@gmail.com>
2021-07-01 17:44:31 +0100

    Parsing data and check for errors

--- philosophers.c
+++ philosophers.c
@@ -6,25 +6,74 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 18:11:11 by mbari             #+#    #+#             */
-/*   Updated: 2021/07/01 14:23:38 by mbari            ###   ########.fr       */
+/*   Updated: 2021/07/01 17:42:28 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int	ft_error_put(char *messsage, int ret)
+{
+	printf("%s\n", messsage);
+	return(ret);
+}
+int	ft_parsing(char **av, t_simulation *simulation)
+{
+	int				num;
+	int				i;
+	int				j;
+
+	i = 1;
+	j = 0;
+	while (av[i])
+	{
+		j = 0;
+		num = 0;
+		while (av[i][j])
+		{
+			if (av[i][j] >= '0' && av[i][j] <= '9')
+				 num = num * 10 + (av[i][j] - '0');
+			else
+				return (ft_error_put("Error: Number Only", 1));
+			j++;
+		}
+		if (i == 1)
+			simulation->philo_numbers = num;
+		else if (i == 2)
+			simulation->time_to_die = num;
+		else if (i == 3)
+			simulation->time_to_eat = num;
+		else if (i == 4)
+			simulation->time_to_sleep = num;
+		else if (i == 5)
+			simulation->notepme = num;
+		i++;
+	}
+	return (0);
+}
+
 int main(int ac, char **av)
 {
 	int i;
+	t_simulation	simulation;
 
 	i = 1;
-
-	printf("|%d|test\n", ac);
-	if (ac != 5 || ac != 6)
-		return (0);
-	while (av[i])
+	printf("|%d|\n", ac);
+		// return (0);
+	if (ac == 5 || ac == 6)
 	{
-		printf("|%d ==> %s|\n", i, av[i]);
-		i++;
+		while (av[i])
+		{
+			printf("|%d ==> %s|\n", i, av[i]);
+			i++;
+		}
+		if (ft_parsing(av, &simulation))
+			return (1);
+		printf("|*************************|\n");
+		printf("|1 : %d|2 : %d|3 : %d|4 : %d|5 : %d|\n", 
+				simulation.philo_numbers, simulation.time_to_die, 
+				simulation.time_to_eat, simulation.time_to_sleep, 
+				simulation.notepme);
 	}
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,9 +6,32 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 18:11:38 by mbari             #+#    #+#             */
-/*   Updated: 2021/06/30 18:11:59 by mbari            ###   ########.fr       */
+/*   Updated: 2021/07/01 17:33:45 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
 #include <stdio.h>
 #include <string.h>
+
+typedef	struct	s_philo
+{
+	int	index;
+	int	right_hand;
+	int	left_hand;
+	int	notepme;
+}				t_philo;
+
+typedef	struct	s_simulation
+{
+	int	philo_numbers;
+	int	forks;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	notepme;
+}				t_simulation;
+
+#endif
