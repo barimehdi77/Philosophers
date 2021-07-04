eb6c1ea7fa3c97f12203ed3db35d082732e22444
barimehdi77 <Barimehdi777@gmail.com>
2021-07-04 18:38:49 +0100

    add pthread_t to hthe struct in header file

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 18:11:11 by mbari             #+#    #+#             */
-/*   Updated: 2021/07/01 17:42:28 by mbari            ###   ########.fr       */
+/*   Updated: 2021/07/04 18:37:58 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 int	ft_error_put(char *messsage, int ret)
 {
 	printf("%s\n", messsage);
-	return(ret);
+	return (ret);
 }
+
 int	ft_parsing(char **av, t_simulation *simulation)
 {
 	int				num;
@@ -52,28 +53,16 @@ int	ft_parsing(char **av, t_simulation *simulation)
 	return (0);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int i;
+	int				i;
 	t_simulation	simulation;
 
 	i = 1;
-	printf("|%d|\n", ac);
-		// return (0);
 	if (ac == 5 || ac == 6)
 	{
-		while (av[i])
-		{
-			printf("|%d ==> %s|\n", i, av[i]);
-			i++;
-		}
 		if (ft_parsing(av, &simulation))
 			return (1);
-		printf("|*************************|\n");
-		printf("|1 : %d|2 : %d|3 : %d|4 : %d|5 : %d|\n", 
-				simulation.philo_numbers, simulation.time_to_die, 
-				simulation.time_to_eat, simulation.time_to_sleep, 
-				simulation.notepme);
 	}
 	return (0);
 }
