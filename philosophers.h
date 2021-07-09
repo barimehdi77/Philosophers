909bef65b2cc57730f860d4478f686aeb219ce5c
barimehdi77 <Barimehdi777@gmail.com>
2021-07-09 13:47:46 +0100

    fix some bugs

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 18:11:38 by mbari             #+#    #+#             */
-/*   Updated: 2021/07/05 20:18:10 by mbari            ###   ########.fr       */
+/*   Updated: 2021/07/09 13:44:36 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,15 @@ typedef struct s_philo
 
 typedef struct s_simulation
 {
-	pthread_t	*threads;
-	//t_philo		*philo;
-	int			philo_index;
-	int			philo_numbers;
-	int			forks;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			eat_counter;
+	pthread_t			*threads;
+	t_philo				*philo;
+	int					philo_index;
+	int					philo_numbers;
+	int					forks;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					eat_counter;
 }				t_simulation;
 
 #endif
